650c23d5c3897df6946a351dca5806ccf0108b1b
PhanThao84 <phanthithanhthao842004123@gmail.com>
2024-01-05 12:15:12 +0700

    Project three

---
+++ B1C5/Program.cs
@@ -0,0 +1,76 @@
+﻿// using System;
+// class Hinhtron
+// {
+//     public double r;
+//     public double d
+//     {
+//         get{return 2*r;}
+//     }
+//     public double S
+//     {
+//         get{return Math.PI*Math.Pow(r,2);}
+//     }
+// }
+// class hinhcau: Hinhtron
+// {
+//     public new double S
+//     {
+//         get{return 4*Math.PI*Math.Pow(r,2);}
+//     }
+//     public double V
+//     {
+//         get{return (4.0/3.0)*Math.PI*Math.Pow(r,3);}
+//     }
+
+//     public void Xuat()
+//     {
+//     Console.WriteLine("Dien tich hinh cau = {0}",S);
+//     Console.WriteLine("The tich hinh cau = {0}",V);
+//     }
+// }
+// class hinhtru: Hinhtron
+// {
+//     public double H;
+//     public double Chuvihinhtru
+//     {
+//         get{return 2*Math.PI*r;}
+//     }
+//     public double Dientichxungquanh
+//     {
+//         get{return 2*Math.PI*r*H;}
+//     }
+//     public double Dientichtoanphan
+//     {
+//         get{return 2*Math.PI*r*(r+H);}
+//     }
+//     public double Thetich
+//     {
+//         get{return Math.PI*Math.Pow(r,2)*H;}
+//     }
+//     public void Xuathinhtru()
+//     {
+//         Console.WriteLine("Chu vi hinh tru = {0}",Chuvihinhtru);
+//         Console.WriteLine("Dien tich xung quanh = {0}",Dientichxungquanh);
+//         Console.WriteLine("Dien tich toan phan  = {0}",Dientichtoanphan);
+//         Console.WriteLine("The tich = {0}",Thetich);
+//     }
+// }
+// class Program 
+// {
+//     static void Main (string [] agrs)
+//     {
+//         hinhcau hinh1 = new hinhcau();
+//         Console.Write("Nhap ban kinh cua hinh cau: ");
+//         double a = double.Parse(Console.ReadLine());
+//         hinh1.r=a;
+//         hinhtru hinh2 = new hinhtru();
+//         Console.Write("Nhap ban kinh cua +hinh tru: ");
+//         double b= double.Parse(Console.ReadLine());
+//         Console.Write("Nhap chieu cao cua hinh tru: ");
+//         double h= double.Parse(Console.ReadLine());
+//         hinh2.r=b;
+//         hinh2.H=h;
+//         hinh1.Xuat();
+//         hinh2.Xuathinhtru();
+//     }
+// }
---
+++ B1C6/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+class Hinhhoc
+{
+    public double a,b,c,cd,cr,r,canh;
+    public virtual void Chuvi()
+    {
+        Console.WriteLine("Chu vi : ");
+    }
+    public virtual void Dientich()
+    {
+        Console.WriteLine("Dien tich : ");
+    }
+}
+class Hinhchunhat:Hinhhoc
+{
+    public override void Chuvi()
+    {
+         Console.WriteLine("Chu vi hcn : {0}",(cd+cr)*2);
+    }
+     public override void Dientich()
+    {
+        Console.WriteLine("Dien tich hcn : {0}",cd*cr);
+    }
+}
+class Hinhtron:Hinhhoc
+{
+    public override void Chuvi()
+    {
+         Console.WriteLine("Chu vi hinhtron : {0}",2*Math.PI*r);
+    }
+     public override void Dientich()
+    {
+        Console.WriteLine("Dien tich hinhtron : {0}",Math.PI*Math.Pow(r,2));
+    }
+}
+class Hinhtamgiac:Hinhhoc
+{
+    public override void Chuvi()
+    {
+         Console.WriteLine("Chu vi hinhtamgiac : {0}",a+b+c);
+    }
+     public override void Dientich()
+    {
+        double p = (a+b+c)/2;
+        Console.WriteLine("Dien tich hinhtamgiac : {0}",Math.Sqrt(p*(p-a)*(p-b)*(p-c)));
+    }
+}
+class Hinhvuong:Hinhhoc
+{
+    public override void Chuvi()
+    {
+         Console.WriteLine("Chu vi hinhvuong : {0}",canh*4);
+    }
+     public override void Dientich()
+    {
+        Console.WriteLine("Dien tich hinhvuong : {0}",canh*canh);
+    }
+}
+class Program 
+{
+    static void Main(string[] agrs)
+    {
+        Hinhchunhat hcn = new Hinhchunhat();
+        Hinhtron ht1 = new Hinhtron();
+        Hinhtamgiac tg = new Hinhtamgiac();
+        Hinhvuong hv1 = new Hinhvuong();
+        Console.Write("Nhap chieu dai hcn: ");
+        double cd1 = double.Parse(Console.ReadLine());
+        Console.Write("Nhap chieu rong hcn: ");
+        double cr1 = double.Parse(Console.ReadLine());
+        hcn.cd = cd1;
+        hcn.cr=cr1;
+        Console.Write("Nhap canh thu 1 cua tam giac : ");
+        double a1 = double.Parse(Console.ReadLine());
+        Console.Write("Nhap canh thu 2 cua tam giac : ");
+        double a2 = double.Parse(Console.ReadLine());
+        Console.Write("Nhap canh thu 3 cua tam giac : ");
+        double a3 = double.Parse(Console.ReadLine());
+        tg.a=a1;
+        tg.b=a2;
+        tg.c=a3;
+        Console.Write("Nhap canh cua hinh vuong : ");
+        double canh1 = double.Parse(Console.ReadLine());
+        hv1.canh=canh1;
+        Console.Write("Nhap ban kinh cua hinh tron: ");
+        double r1= double.Parse(Console.ReadLine());
+        ht1.r=r1;
+        hcn.Chuvi();
+        hcn.Dientich();
+        ht1.Chuvi();
+        ht1.Dientich();
+        tg.Chuvi();
+        tg.Dientich();
+        hv1.Chuvi();
+        hv1.Dientich();
+    }
+}
